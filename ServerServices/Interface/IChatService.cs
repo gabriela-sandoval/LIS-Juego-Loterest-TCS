c894167f1ad80d5a14d98aebb145234ff7874c5e
Samuel Gaona <azureussme@gmail.com>
2019-11-10 17:55:53 -0600

    Más interfaces y documentación en los servicios Login y Chat

--- ServerServices/Interface/IChatService.cs
+++ ServerServices/Interface/IChatService.cs
@@ -1,14 +1,48 @@
+using System;
+using System.Collections.Generic;
 using System.ServiceModel;
 
 namespace ServerServices.Interface
 {
-    [ServiceContract(CallbackContract = typeof(IChatServiceCallback))]
+    /// <summary>
+    ///     Contrato para el servicio Chat
+    /// </summary>
+    [ServiceContract(CallbackContract = typeof(IChatServiceCallback), SessionMode = SessionMode.Required)]
     public interface IChatService
     {
+        /// <summary>
+        ///     Realiza una solicitud de conexión al chat, proporcionando la id de sesión.
+        /// </summary>
+        /// <param name="sessionId">Id de la sesión del cliente</param>
         [OperationContract(IsOneWay = true)]
-        void Connect(string userGuid);
-        
+        void Connect(string sessionId);
+
+        /// <summary>
+        ///     Realiza una solicitud de conexión al chat, proporcionando un nombre de usuario.
+        /// </summary>
+        /// <param name="username">Nombre de usuario</param>
+        /// <returns>Id de la sesión del cliente</returns>
+        [Obsolete]
+        [OperationContract]
+        string ConnectWithUsername(string username);
+
+        [OperationContract]
+        List<string> GetConnectedUsers();
+
+        /// <summary>
+        ///     Envía un mensaje al chat, proporcionando la id de sesión. Envía a todos los usuarios conectados
+        ///     el mensaje, excepto quien lo envía.
+        /// </summary>
+        /// <param name="sessionId">Id de la sesión del cliente</param>
+        /// <param name="message">Mensaje del cliente</param>
         [OperationContract(IsOneWay = true)]
-        void SendMessage(string userGuid, string message);
+        void SendMessage(string sessionId, string message);
+
+        /// <summary>
+        ///     Realiza una solicitud de desconexión del chat, proporcionando la id de sesión.
+        /// </summary>
+        /// <param name="sessionId">Id de la sesión del cliente</param>
+        [OperationContract]
+        void Disconnect(string sessionId);
     }
 }
--- ServerServices/Interface/ILoginServiceCallback.cs
+++ ServerServices/Interface/ILoginServiceCallback.cs
@@ -1,14 +1,42 @@
 using System.ServiceModel;
+using ServerServices.Model.Enum;
 
 namespace ServerServices.Interface
 {
+    /// <summary>
+    ///     Contrato para el callback del servicio Login.
+    /// </summary>
     [ServiceContract]
     public interface ILoginServiceCallback
     {
+        /// <summary>
+        ///     Evento que sucede cuando el inicio de sesión fue exitoso.
+        /// </summary>
+        /// <param name="sessionId">Id de la sesión del cliente</param>
         [OperationContract(IsOneWay = true)]
-        void OnLoginSuccess(string guid);
-        
+        void OnLoginSuccess(string sessionId);
+
+        /// <summary>
+        ///     Evento que sucede cuando hubo un error al realizar un inicio de sesión.
+        /// </summary>
+        /// <param name="replyCode">Código de respuesta</param>
+        /// <param name="message">Mensaje del error</param>
         [OperationContract(IsOneWay = true)]
-        void OnLoginError(string message);
+        void OnLoginError(LoginRequestReplyCode replyCode, string message);
+
+        /// <summary>
+        ///     Evento que sucede cuando hay una respuesta a la solicitud de registro de usuario.
+        /// </summary>
+        /// <param name="replyCode">Código de respuesta</param>
+        /// <param name="message">Mensaje de respuesta</param>
+        [OperationContract(IsOneWay = true)]
+        void OnRegisterResponse(RegisterRequestReplyCode replyCode, string message);
+
+        /// <summary>
+        ///     Evento que sucede cuando hay una respuesta a la solicitud de verificación.
+        /// </summary>
+        /// <param name="replyCode">Código de respuesta</param>
+        [OperationContract(IsOneWay = true)]
+        void OnVerificationResponse(VerificationRequestReplyCode replyCode);
     }
 }
