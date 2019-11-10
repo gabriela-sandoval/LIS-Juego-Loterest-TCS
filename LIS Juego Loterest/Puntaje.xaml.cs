ecd5f1e53eba4398219c026ecd757dcc641d60e6
Gabriela Sandoval Cruz <gabriela.uv@outlook.com>
2019-11-10 13:09:49 -0600

    Ventana Puntaje
    Creción de ventana puntaje

---
+++ LIS Juego Loterest/Puntaje.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LIS_Juego_Loterest
+{
+    /// <summary>
+    /// Lógica de interacción para Puntaje.xaml
+    /// </summary>
+    public partial class Puntaje : Window
+    {
+        public Puntaje()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBoxUserName_Copy1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
