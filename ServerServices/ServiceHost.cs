6bcd2b03b62235a6e074a56420b94f0d31d92ad2
Samuel Gaona <azureussme@gmail.com>
2019-11-13 11:58:33 -0600

    Prueba de uso de DatabaseContext

--- ServerServices/ServiceHost.cs
+++ ServerServices/ServiceHost.cs
@@ -1,9 +1,20 @@
+using System;
+using System.Linq;
 using System.ServiceModel;
+using DataModel;
 
 namespace ServerServices
 {
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public partial class ServiceHost
     {
+        private DatabaseContext _database;
+        
+        public ServiceHost()
+        {
+            _database = new DatabaseContext();
+            var jugadores = _database.Jugador.ToArray();
+            Console.WriteLine($"Hay {jugadores.Length} en la base de datos.");
+        }
     }
 }
