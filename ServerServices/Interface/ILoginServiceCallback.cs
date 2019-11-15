e2fca1c810410496e624b533547a5d0eb7ac092e
Samuel Gaona <azureussme@gmail.com>
2019-11-15 14:54:36 -0600

    Se divide el Callback de LoginService en Login, Register y Verification

--- ServerServices/Interface/ILoginServiceCallback.cs
+++ ServerServices/Interface/ILoginServiceCallback.cs
@@ -23,20 +23,5 @@ namespace ServerServices.Interface
         /// <param name="message">Mensaje del error</param>
         [OperationContract(IsOneWay = true)]
         void OnLoginError(LoginRequestReplyCode replyCode, string message);
-
-        /// <summary>
-        ///     Evento que sucede cuando hay una respuesta a la solicitud de registro de usuario.
-        /// </summary>
-        /// <param name="replyCode">Código de respuesta</param>
-        /// <param name="message">Mensaje de respuesta</param>
-        [OperationContract(IsOneWay = true)]
-        void OnRegisterResponse(RegisterRequestReplyCode replyCode, string message);
-
-        /// <summary>
-        ///     Evento que sucede cuando hay una respuesta a la solicitud de verificación.
-        /// </summary>
-        /// <param name="replyCode">Código de respuesta</param>
-        [OperationContract(IsOneWay = true)]
-        void OnVerificationResponse(VerificationRequestReplyCode replyCode);
     }
 }
