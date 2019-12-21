3dca80f345741cf1ceb9fa3bf9babc7086be607a
Gabriela Sandoval Cruz <gabriela.uv@outlook.com>
2019-12-21 12:36:50 -0600

    Cambio de página Loteria
    Cambio de página por ventana

--- LIS Juego Loterest/Ventanas/Loteria.xaml.cs
+++ LIS Juego Loterest/Ventanas/Loteria.xaml.cs
@@ -1,17 +1,13 @@
 ﻿using System;
 using System.Windows;
-using LIS_Juego_Loterest.Interface;
-using LIS_Juego_Loterest.Páginas;
 
 namespace LIS_Juego_Loterest
 {
     /// <summary>
     /// Lógica de interacción para Loteria.xaml
     /// </summary>
-    public partial class Loteria : IPageListener
+    public partial class Loteria : Window
     {
-        private IPageManager _pageManager;
-        
         public Loteria()
         {
             InitializeComponent();
@@ -19,29 +15,17 @@ namespace LIS_Juego_Loterest
 
         private void ButtonRegresar_Click(object sender, RoutedEventArgs e)
         {
-            _pageManager.CambiarPantalla<Menú>();
+            
         }
 
         private void ButtonLoteria_Click(object sender, RoutedEventArgs e)
         {
-            _pageManager.CambiarPantalla<Ganaste>();
-        }
-
-        public void SetPageManager(IPageManager pageManager)
-        {
-            _pageManager = pageManager;
+           
         }
 
         private void Loteria1_Loaded(object sender, RoutedEventArgs e)
         {
             
-        }
-
-        private int increment = 0;
-        private void DispatcherTimerTickSeconds(object sender, EventArgs e)
-        {
-            increment++;
-            TiempoLabel.Content = increment.ToString();
         }
     }
 }
